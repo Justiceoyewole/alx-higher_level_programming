14adc0007d9b13a5612af124228bd1ca417126a9
root <justiceoyewo@gmail.com>
2024-08-22 12:01:16 -0700

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+def uppercase(str):
+    result = ""
+    for char in str:
+        # Check if the character is a lowercase letter
+        if 'a' <= char <= 'z':
+            # Convert lowercase to uppercase by subtracting 32
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print("{}".format(result))
